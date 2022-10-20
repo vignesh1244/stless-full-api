e29c453acfe188c332d6b2651713561ec4bd98fa
vignesh <vigneshsm1244@gmail.com>
2022-10-20 17:10:37 +0530

    imagecommitqa

--- lib/views/stless.dart
+++ lib/views/stless.dart
@@ -10,7 +10,7 @@ class HomepageP extends StatelessWidget {
     print('builds');
     return Scaffold(
       appBar: AppBar(
-        title: Text('Stateless example abcd'),
+        title: Text('Stateless example abcde'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
